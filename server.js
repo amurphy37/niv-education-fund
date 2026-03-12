8467040f2e76f0a36b58264f1736d0beebc73574
amurphy37 <amurphy37@gmail.com>
2026-03-11 22:34:16 -0700

    updating

--- server.js
+++ server.js
@@ -17,14 +17,14 @@ const routes = require("./api")
 app.use('/', routes);
 
 // Production Mode
-if (process.env.NODE_ENV === "production") {
-    console.log(process.env.NODE_ENV)
-    app.use(express.static('client/build'))
+// if (process.env.NODE_ENV === "production") {
+//     console.log(process.env.NODE_ENV)
+//     app.use(express.static('client/build'))
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "/client/build/index.html"))
-    })
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname, "/client/build/index.html"))
+//     })
+// }
 
 // Tell the user which port app is running on when successfully up and running.
 app.listen(PORT, console.log(`Server is starting at ${PORT}`))
